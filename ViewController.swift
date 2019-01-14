44cd39911c6737063d410047d2e1941afffd8db2
cagdaseksi <cagdas.eksi@hotmail.com>
2019-01-14 22:56:58 +0300

    Initial commit

---
+++ ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  QrCodeGenerator
+//
+//  Created by MAC on 14/1/19.
+//  Copyright © 2019 cagdaseksi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    @IBAction func button(_ sender: Any)
+    {
+        if let myString = myTextField.text
+        {
+            let data = myString.data(using: .ascii, allowLossyConversion: false)
+            let filter = CIFilter(name: "CIQRCodeGenerator")
+            filter?.setValue(data, forKey: "inputMessage")
+            
+            let img = UIImage(ciImage: (filter?.outputImage)!)
+            
+            myImageView.image = img
+        }
+    }
+
+
+}
+
